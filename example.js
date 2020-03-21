32d2c60846ecf73c2f2fc29dd55d52cb65357a1d
Jannis R <mail@jannisr.de>
2020-03-21 01:11:21 +0100

    WIP

--- example.js
+++ example.js
@@ -1,3 +1,47 @@
 'use strict'
 
-// todo
+const createHafasClient = require('hafas-client')
+const withThrottling = require('hafas-client/throttle')
+const bvgProfile = require('hafas-client/p/bvg')
+const generateGtfs = require('.')
+
+const centerOfBerlin = {
+	type: 'Polygon',
+	coordinates: [[
+		[13.350, 52.528],
+		[13.350, 52.516],
+		[13.358, 52.504],
+		[13.389, 52.493],
+		[13.436, 52.495],
+		[13.449, 52.513],
+		[13.439, 52.530],
+		[13.413, 52.537],
+		[13.378, 52.538],
+		[13.350, 52.528]
+	]]
+}
+
+// todo: with retrying
+const hafas = createHafasClient(
+	withThrottling(bvgProfile, 5, 1000), // 5/s
+	'hafas-generate-gtfs:example',
+)
+
+const createWritable = (filename) => {
+	const write = (row, _, cb) => {
+		console.log(filename, row)
+		cb(null)
+	}
+	return new Writable({objectMode: true, write})
+}
+
+;(async () => {
+	await generateGtfs(hafas, createWritable, centerOfBerlin, {
+		begin: new Date('2020-06-01T03:00+02:00'),
+		duration: 2 * 24 * 60 * 60 * 1000,
+	})
+})()
+.catch((err) => {
+	console.error(err)
+	process.exit(1)
+})
--- index.js
+++ index.js
@@ -1,3 +1,67 @@
 'use strict'
 
-// todo
+const {ok} = require('assert')
+const findStations = require('hafas-find-stations')
+const debug = require('debug')('hafas-generate-gtfs')
+const createCollectDeps = require('hafas-collect-departures-at')
+const findDepsDurationLimit = require('./lib/find-departures-duration-limit')
+
+const DAY = 24 * 60 * 60 * 1000
+
+const isValidDep = (dep) => {
+	const when = +new Date(dep.plannedWhen)
+	if (Number.isNaN(when)) {
+		debug('invalid departure', dep)
+		return false
+	}
+	return true
+}
+
+// todo: progress estimation
+const generateGtfs = async (hafas, createWritable, serviceArea, opt = {}) => {
+	let {
+		begin, duration,
+		// todo: products filter?
+	} = {
+		begin: Date.now(),
+		duration: 30 * DAY,
+		...opt
+	}
+	begin = +new Date(begin)
+	ok(!Number.isNaN(begin), 'begin is invalid')
+	ok(Number.isInteger(duration), 'duration is invalid')
+	ok(duration > 0, 'duration is invalid')
+
+	// todo: optionally use hafas-discover-stations?
+	const stations = await findStations(hafas, serviceArea)
+
+	const durStep = await findDepsDurationLimit(hafas, stations[0])
+	debug('duration step', durStep)
+
+	const onDep = (dep, station) => {
+		// todo
+		console.log(JSON.stringify([dep, station]))
+	}
+
+	const end = begin + duration
+	const collectDeps = createCollectDeps(hafas.departures, {}, durStep)
+	for (const station of stations) {
+		debug('fetching departures', station.id, station.name)
+		for await (let deps of collectDeps(station.id, begin)) {
+			deps = deps
+			.filter(isValidDep)
+			.sort((a, b) => new Date(a.plannedWhen) - new Date(b.plannedWhen))
+			deps.forEach(dep => onDep(dep, station))
+
+			const lastWhen = +new Date(deps[deps.length - 1].plannedWhen)
+			if (deps.length === 0) {
+				debug('0 departures', deps) // todo: log `when`
+				break
+			} else if (lastWhen >= end) break
+			else debug('progress', ((lastWhen - begin) / duration).toFixed(3))
+		}
+	}
+	// todo
+}
+
+module.exports = generateGtfs
